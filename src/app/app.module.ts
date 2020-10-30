b279beccfdff47c063861e70b1efee7ceb355106
Tushar Sharma <nupursharma@MacBook-Air.local>
2020-10-30 23:22:07 +0530

    initial commet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
